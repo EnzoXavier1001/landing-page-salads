ae54b24368af4278e6c0e76b1f0ae77554d81eed
Enzo Costa <enzo_costa7@hotmail.com>
2022-03-19 12:43:31 -0300

    Create the section share

--- main.js
+++ main.js
@@ -13,3 +13,20 @@ for (const link of links) {
         nav.classList.remove('show-menu')
     })
 }
+
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+  })
+  
+  scrollReveal.reveal(
+    `.home .home__container .home__img, .home .home__container .home__data,
+    .about .section-title, .about .section-subtitle, .about .about__container,
+    .cardapio .section-title, .cardapio .section-subtitle, .cardapio .cardapio__container,
+    .contact .section-title, .contact .section-subtitle, .contact .contact__container,
+    .delivery .delivery__container
+    `,
+    { interval: 100 }
+  )
