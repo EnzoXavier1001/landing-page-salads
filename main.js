a6c7d82d02fd37b8311065dfe4c031b926babeee
Enzo Costa <enzo_costa7@hotmail.com>
2022-03-19 11:33:53 -0300

    Create the section menu

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 // show menu 
+const toggle = document.querySelector('#nav-toggle')
+const nav = document.querySelector('#nav-menu')
 
-const showMenu = (toggleId, navId) => {
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+toggle.addEventListener('click', function() {
+    nav.classList.toggle('show-menu')
+})
 
-    if(toggle && nav) {
-        toggle.addEventListener('click', () => {
-            nav.classList.toggle('show-menu')
-        })
-    }
+const links = document.querySelectorAll('.nav__link')
+
+for (const link of links) {
+    link.addEventListener('click', function() {
+        nav.classList.remove('show-menu')
+    })
 }
-
-showMenu('nav-toggle', 'nav-menu')
